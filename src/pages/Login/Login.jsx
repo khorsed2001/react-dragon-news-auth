b21a5847952e7552ad73d2fc1d5e02ce2b9de687
khorsed2001 <khorsedranabd.gmail.com>
2024-08-21 10:39:13 +0600

    name add

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ const Login = () => {
         <div>
             <Navbar></Navbar>
             <h2 className="text-3xl">This is login</h2>
+            <h3 className="text-3xl">My name is khorsed</h3>
         </div>
     );
 };
